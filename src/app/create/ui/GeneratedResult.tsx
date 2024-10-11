532733c7e7c490f10f7e3dfd979ecc786b1325e1
claire.barnaby <clb.miclairy@gmail.com>
2024-10-11 17:36:49 +0200

    prettier generated image

--- src/app/create/ui/GeneratedResult.tsx
+++ src/app/create/ui/GeneratedResult.tsx
@@ -8,10 +8,19 @@ export const GeneratedResult = ({ imageUrl }: Props) => {
         return null;
     }
     return (
-        <div>
-            <h3>You land again back on the precipice all you are left with is a picture for your journal.</h3>
-            <img src={imageUrl} alt="dragon" width={1024} height={1024} />
-            <p>
+        <div className=" mt-4 max-w-3xl h-3xl mx-auto rounded-xl border-dashed border-blue border-2 ">
+            <h3 className="p-2 text-lg">
+                You land again back on the precipice all you are left with is a picture for your journal.
+            </h3>
+            <img
+                className="border-white border-solid border-4"
+                style={{ transform: 'rotate(2deg)' }}
+                src={imageUrl}
+                alt="dragon"
+                width={1024}
+                height={1024}
+            />
+            <p className="text-sm p-2 my-4">
                 Congratulations you survived here is your dragon. Adoption is in sponsorship only. You reserve no rights
                 to the dragon in life or death. Thank you for helping protect these majestic creatures. T&Cs apply
             </p>
