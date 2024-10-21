7ea21523fb6f4a1123ea90a65d01f0eebbf5f030
claire.barnaby <clb.miclairy@gmail.com>
2024-10-21 17:40:15 +0200

    change default

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -5,4 +5,4 @@ export const LIMIT = 4;
 export const GALLERY = '/gallery';
 
 export const defaultImage =
-    'https://dragon-images.s3.eu-north-1.amazonaws.com/2cf12c4b-cd89-4a3c-84c1-a033fa415231.png';
+    'https://dragon-images.s3.eu-north-1.amazonaws.com/12221e3f-6f35-4dbb-b072-2648c2671f1c.png';
